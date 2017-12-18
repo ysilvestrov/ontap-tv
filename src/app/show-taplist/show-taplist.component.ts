b200e842b520ad932df16e2af3a730065ef2a02c
Yuriy Silvestrov <yuriy@silvestrov.com>
2017-12-18 22:59:26 +0200

    msg

--- src/app/show-taplist/show-taplist.component.ts
+++ src/app/show-taplist/show-taplist.component.ts
@@ -6,6 +6,7 @@ import {TaplistService} from '../taplist.service';
 import {ActivatedRoute, ParamMap, Router} from '@angular/router';
 import 'rxjs/add/operator/switchMap';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/timer';
 
 @Component({
   selector: 'app-show-taplist',
@@ -15,9 +16,12 @@ import {Observable} from 'rxjs/Observable';
 export class ShowTaplistComponent implements OnInit {
 
   id: string;
+  originalTaplist: ITaplistItem[];
   taplist: ITaplistItem[];
   settings: ITaplistSettings;
   objectKeys = Object.keys;
+  startPosition: number = 0;
+  timer: Observable<number>;
 
   constructor(
     private route: ActivatedRoute,
@@ -29,11 +33,32 @@ export class ShowTaplistComponent implements OnInit {
   }
 
   getTaplist(): void {
-    this.taplistService.get(this.id).subscribe(taplist => this.taplist = taplist);
+    this.taplistService.get(this.id).subscribe(taplist => {
+      this.originalTaplist = taplist;
+      this.timer = Observable.timer(3000, 10000);
+      this.timer.subscribe(_ => this.processTaplist());
+      return taplist;
+    });
   }
 
   getSettings(): void {
     this.settingsService.get(this.id).subscribe(settings => {this.settings = settings; this.getTaplist()});
+  }
+
+  processTaplist(){
+    let start: number = 0;
+    let end: number = this.originalTaplist.length;
+    if (end > 16 ) {
+      start = this.startPosition;
+      end = start + 16;
+      this.startPosition = end;
+      if (end > this.originalTaplist.length) {
+        end = this.originalTaplist.length;
+        start = end - 16;
+        this.startPosition = 0;
+      }
+    }
+    this.taplist = this.originalTaplist.slice(start, end);
   }
 
   ngOnInit() {
